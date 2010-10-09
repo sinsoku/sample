bb5ae2d40381e7f68e831cab823fc0bc6ac74cf7
sinsoku <sinsoku.listy@gmail.com>
2010-10-09 18:09:49 +0900

    色々な値で初期化できるコンストラクタを実装

--- pystr/src/main/java/sinsoku/sample/Pystr.java
+++ pystr/src/main/java/sinsoku/sample/Pystr.java
@@ -3,6 +3,33 @@ package sinsoku.sample;
 public class Pystr {
 	String str = "";
 
+	public Pystr() {
+	}
+
+	public Pystr(String s) {
+		this.str = s;
+	}
+
+	public Pystr(int i) {
+		this.str = String.valueOf(i);
+	}
+
+	public Pystr(long l) {
+		this.str = String.valueOf(l);
+	}
+
+	public Pystr(double d) {
+		this.str = String.valueOf(d);
+	}
+
+	public Pystr(char c) {
+		this.str = String.valueOf(c);
+	}
+
+	public Pystr(char[] data) {
+		this.str = String.valueOf(data);
+	}
+
 	@Override
 	public String toString() {
 		return str;
--- pystr/src/test/java/sinsoku/sample/PystrTest.java
+++ pystr/src/test/java/sinsoku/sample/PystrTest.java
@@ -8,7 +8,35 @@ import org.junit.Test;
 public class PystrTest {
 	@Test
 	public void インスタンスを新規作成() throws Exception {
-		Pystr pystr = new Pystr();
-		assertThat(pystr.toString(), is(""));
+		Pystr str = new Pystr();
+		assertThat(str.toString(), is(""));
+	}
+
+	@Test
+	public void インスタンスを初期化して作成() throws Exception {
+		// 文字列で初期化
+		Pystr str = new Pystr("abc");
+		assertThat(str.toString(), is("abc"));
+
+		// 整数で初期化
+		Pystr intstr = new Pystr(0);
+		assertThat(intstr.toString(), is("0"));
+
+		// longで初期化
+		Pystr lstr = new Pystr(10000000000L);
+		assertThat(lstr.toString(), is("10000000000"));
+
+		// 実数で初期化
+		Pystr doublestr = new Pystr(0.0);
+		assertThat(doublestr.toString(), is("0.0"));
+
+		// 文字で初期化
+		Pystr charstr = new Pystr('a');
+		assertThat(charstr.toString(), is("a"));
+
+		// 文字配列で初期化
+		char[] charr = {'a', 'a', 'a'};
+		Pystr carrstr = new Pystr(charr);
+		assertThat(carrstr.toString(), is("aaa"));
 	}
 }
