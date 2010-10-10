96f23b420eaddc019cbece58be7599e4cbccd620
sinsoku <sinsoku.listy@gmail.com>
2010-10-11 00:31:10 +0900

    codePointBeforeのJavaの動作を確認

--- pystr/src/test/java/sinsoku/sample/StringTest.java
+++ pystr/src/test/java/sinsoku/sample/StringTest.java
@@ -17,4 +17,15 @@ public class StringTest {
 		assertThat(str.codePointAt(4), is(12354));
 		assertThat(str.codePointAt(5), is(34920));
 	}
+
+	@Test
+	public void test_codePointBefore() throws Exception {
+		String str = "0z?!あ表";
+		assertThat(str.codePointBefore(1), is(48));
+		assertThat(str.codePointBefore(2), is(122));
+		assertThat(str.codePointBefore(3), is(63));
+		assertThat(str.codePointBefore(4), is(33));
+		assertThat(str.codePointBefore(5), is(12354));
+		assertThat(str.codePointBefore(6), is(34920));
+	}
 }
