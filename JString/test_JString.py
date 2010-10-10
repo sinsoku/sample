a93bde72249ba77e0a948f9cc68bd31c9e31a978
sinsoku <sinsoku.listy@gmail.com>
2010-10-10 22:55:47 +0900

    unicodeでも動作を確認

--- JString/test_JString.py
+++ JString/test_JString.py
@@ -12,3 +12,7 @@ class TestJString(object):
     def test_init_JString_str(self):
         eq_(str(JString('')), '')
         eq_(str(JString('abc')), 'abc')
+
+    def test_init_JString_ustr(self):
+        eq_(str(JString(u'')), '')
+        eq_(str(JString(u'abc')), 'abc')
